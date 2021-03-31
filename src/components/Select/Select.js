9dac1d8b640c03fb04e65971df0353184c882946
willdebras <wtb3na@virginia.edu>
2021-03-31 18:33:51 -0500

    adding progress bar

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,70 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+
+    <Wrapper>
+    <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </NativeSelect>
+
+    <PresText>
+      {displayedValue}
+      <IconWrapper style={{'--size': 24 + 'px'}}>
+      <Icon id="chevron-down" strokeWidth={1} size = {24}></Icon>
+      </IconWrapper>
+    </PresText>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position:relative;
+  width:max-content;
+  isolation:isolate;
+`
+
+const NativeSelect = styled.select`
+  position:absolute;
+  left:0;
+  top:0;
+  width:100%;
+  height:100%;
+  opacity:0;
+  z-index:2;
+
+  
+`
+
+const PresText = styled.div`
+background-color:${COLORS.transparentGray15};
+color:${COLORS.gray700};
+z-index:1;
+font-size: 1rem;
+padding:12px 16px;
+padding-right:52px;
+border-radius:8px;
+/*hack from Josh on setting the outline to the browser focus color*/
+${NativeSelect}:focus + & {
+ 
+  outline: 1px dotted #212121;
+  outline:5px auto -webkit-focus-ring-color;
+}
+
+${NativeSelect}:hover + & {
+  color:black;
+}
+
+`
+
+const IconWrapper = styled.div`
+position:absolute;
+top:0;
+right:10px;
+bottom:0;
+margin:auto;
+height: var(--size);
+width: var(--size);
+/* could use pointer-events:none; so it cant be clicked on despite being in front*/
+`
 
 export default Select;
